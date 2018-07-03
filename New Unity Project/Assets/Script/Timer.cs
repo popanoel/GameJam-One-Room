e6795f0d7836fc7b410f59055b64f422ffabca3a
Vincent Latour <popa-noel@hotmail.com>
2018-07-03 14:50:36 -0400

    0.1 Timer + setup

---
+++ New Unity Project/Assets/Script/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	float _tempsRestant = 300f; // EN SECONDE
+
+	[SerializeField]
+	Text _afficheTemps;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_tempsRestant -= Time.deltaTime;
+
+		float minRestant = Mathf.Floor(_tempsRestant/60);
+		float secRestant = Mathf.Floor(_tempsRestant % 60);
+	
+		_afficheTemps.text = minRestant.ToString() +" : " + secRestant.ToString();
+
+		if(_tempsRestant==0f){
+			Debug.Log("Game Over");
+		}
+	}
+}
