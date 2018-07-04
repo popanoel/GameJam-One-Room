9063f80bf817d75b631aa1f90e9c488242354733
Vincent Latour <popa-noel@hotmail.com>
2018-07-04 17:05:01 -0400

    Revert "Revert "0.2 Bouton log""
    This reverts commit abff4c3e6ed4e3b86d2f88ce3b0791ad1dc86095.

---
+++ New Unity Project/Assets/Script/PanelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour {
+
+	public void goRight(){
+		Debug.Log("Allons a droite");
+	}
+	public void goLeft(){
+		Debug.Log("Allons a gauche");
+	}
+}
---
+++ New Unity Project/Assets/Script/PersoManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoManager : MonoBehaviour {
+
+	public enum Char {Bonnie,Amelia,Bernard,Chips,Joachim,None};
+
+	private Char CurrentChar=Char.None;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Char getCurrentChar{
+		get{
+			return CurrentChar;
+		}
+		set{
+			CurrentChar=value;
+		}
+	}
+}
