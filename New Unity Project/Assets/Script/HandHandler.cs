6195fea30bf6d27077da567975859617a7911a55
Vincent Latour <popa-noel@hotmail.com>
2018-07-09 05:19:39 -0400

    1.1

--- New Unity Project/Assets/Script/HandHandler.cs
+++ New Unity Project/Assets/Script/HandHandler.cs
@@ -13,7 +13,7 @@ public class HandHandler : MonoBehaviour {
 	
 	private void OnMouseEnter()
 	{
-		_maMain.transform.position=new Vector3(_maMain.transform.position.x,-65,_maMain.transform.position.z);
+		_maMain.transform.position=new Vector3(_maMain.transform.position.x,GameObject.Find("Canvas").transform.position.y,_maMain.transform.position.z);
 	}
 
 	private void OnMouseExit()
