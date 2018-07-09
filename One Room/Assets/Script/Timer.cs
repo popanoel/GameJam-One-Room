e0fbbf5c48e6633edb34da3a11f45be721b6b06b
Vincent Latour <popa-noel@hotmail.com>
2018-07-09 05:55:31 -0400

    1.2 Final baton

---
+++ One Room/Assets/Script/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	float _tempsRestant = 360f; // EN SECONDE
+	Text _afficheTemps;
+
+
+
+	// Use this for initialization
+	void Start () {
+		_afficheTemps=GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!GameManager.instance._isPaused){
+			_tempsRestant -= Time.deltaTime;
+
+			float minRestant = Mathf.Floor(_tempsRestant/60);
+			float secRestant = Mathf.Floor(_tempsRestant % 60);
+			string aff_sec = secRestant.ToString();
+			string aff_min = minRestant.ToString();
+			if(secRestant<=9){aff_sec="0"+aff_sec;}
+		
+			_afficheTemps.text = aff_min + " : " + aff_sec;
+
+			if(_tempsRestant<=1f){
+				GameManager.instance.StartCoroutine(GameManager.instance.GameO(PersoManager.Char.Me));
+			}
+
+		}
+	}
+}
